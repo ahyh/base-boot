3c26a1637a6f7409b5521deb98949b9da2663bb6
ahyanhuan <ahyanhuan@sina.com>
2019-06-13 21:42:26 +0800

    pom文件修改

---
+++ yh-boot/src/main/java/com/yh/pojo/Salary.java
@@ -0,0 +1,61 @@
+package com.yh.pojo;
+
+import java.math.BigDecimal;
+
+/**
+ * salary表映射对象
+ *
+ * @author yanhuan
+ */
+public class Salary extends BaseDomain {
+
+    private String name;
+
+    private Integer age;
+
+    private Byte sex;
+
+    private String company;
+
+    private BigDecimal salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
