b583c2ae44e6059333ce03b82b77cc2b180f2d9e
ahyanhuan <ahyanhuan@sina.com>
2019-06-23 19:16:50 +0800

    activemq生产者Demo

---
+++ activemq-producer/src/main/java/com/yh/boot/activemq/producer/ActivemqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.yh.boot.activemq.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqProducerApplication.class, args);
+	}
+
+}
---
+++ activemq-producer/src/main/java/com/yh/boot/activemq/producer/helloworld/ActiveMqProducer.java
@@ -0,0 +1,46 @@
+package com.yh.boot.activemq.producer.helloworld;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * ActiveMq消息生产Demo，用于熟悉ActiveMQ生产消息的流程，学习源码
+ *
+ * @author yanhuan
+ */
+public class ActiveMqProducer {
+
+    /**
+     * ActiveMQ的地址
+     */
+    public static final String ACTIVE_MQ_URL = "tcp://192.168.0.120:61616";
+
+    public static final String DESTINATION = "Producer-Demo";
+
+    public static void main(String[] args) throws Exception {
+        //1-创建ActiveMq的连接工厂
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ACTIVE_MQ_URL);
+        //2-创建连接并启动
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+        //3-通过连接创建会话
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //4-创建队列
+        Queue queue = session.createQueue(DESTINATION);
+        //5-创建生产者
+        MessageProducer producer = session.createProducer(queue);
+        //6-发送3条消息
+        for (int i = 0; i < 3; i++) {
+            //创建TextMessage并发送
+            TextMessage textMessage = session.createTextMessage("TextMessage:" + i);
+            producer.send(textMessage);
+        }
+        System.out.println("Message send success");
+        //7-关闭资源
+        producer.close();
+        session.close();
+        connection.close();
+    }
+
+}
