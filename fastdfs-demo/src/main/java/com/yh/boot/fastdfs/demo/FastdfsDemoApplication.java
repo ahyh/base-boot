d7688233b0eb3f5526f0f731eb0f989c47d552f3
ahyanhuan <ahyanhuan@sina.com>
2019-06-30 11:07:27 +0800

    fastdfs初步搭建

---
+++ fastdfs-demo/src/main/java/com/yh/boot/fastdfs/demo/FastdfsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yh.boot.fastdfs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastdfsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastdfsDemoApplication.class, args);
+	}
+
+}
