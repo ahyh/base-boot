049e8f5068f65919440e2d1e0f5911e528c43784
ahyanhuan <ahyanhuan@sina.com>
2019-07-08 23:58:11 +0800

    zk的jar包引入

---
+++ zk-demo/src/main/java/com/yh/zk/ZkDemo.java
@@ -0,0 +1,8 @@
+package com.yh.zk;
+
+public class ZkDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ zk-demo/src/test/java/com/yh/zk/test/ZkDemoTest.java
@@ -0,0 +1,11 @@
+package com.yh.zk.test;
+
+import org.junit.Test;
+
+public class ZkDemoTest {
+
+    @Test
+    public void test() {
+
+    }
+}
