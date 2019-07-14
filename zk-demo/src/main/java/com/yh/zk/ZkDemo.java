24a7cf8837e496f156e28e413bfc6982f823b880
ahyanhuan <ahyanhuan@sina.com>
2019-07-14 19:40:38 +0800

    zk的Watcher机制

--- zk-demo/src/main/java/com/yh/zk/ZkDemo.java
+++ zk-demo/src/main/java/com/yh/zk/ZkDemo.java
@@ -4,10 +4,23 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.util.List;
+
+/**
+ * 测试ZK
+ *
+ * @author yanhuan
+ */
 public class ZkDemo {
 
     public static void main(String[] args) throws Exception {
-
-
+        ZooKeeper zooKeeper = new ZooKeeper("mini1:2181,mini2:2181,mini3:2181", 3000, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("WatchedEvent:" + event);
+            }
+        });
+        List<String> children = zooKeeper.getChildren("/zk-book", true);
+        children.forEach(System.out::println);
     }
 }
